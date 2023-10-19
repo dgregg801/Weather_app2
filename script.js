a4fe4c2c009f8f2e36bdc27bb0aa0b8509dc3dc4
Darin Gregg <daringregg801@gmail.com>
2023-10-19 08:13:06 -0600

    WeatherAppProjectsUpdate

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+let currentWeather = null
+
+
+
 const weatherForm = document.getElementById('weatherForm');
 const locationInput = document.getElementById('locationInput');
 const weatherInfo = document.getElementById('weatherInfo');
@@ -11,7 +16,6 @@ weatherForm.addEventListener('submit', function (e) {
 });
 
 function getGeoCodingInfo(location) {
-    // Replace 'YOUR_API_KEY' with your actual API key
     const apiKey = '944b444c9a6e0d95a21d8a65f8008860';
     const geoApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`;
     
@@ -61,7 +65,71 @@ function processData(data) {
 function displayWeatherInfo(weatherData) {
     weatherInfo.innerHTML = `
         <h2>Weather in ${weatherData.city}</h2>
-        <p>Temperature: ${weatherData.temperature}°C</p>
+        <p>Temperature: ${weatherData.temperature}°F</p>
         <p>Description: ${weatherData.description}</p>
     `;
 }
+
+class SelectedAreas {
+  constructor() {
+      this.areas = [];
+  }
+
+  addArea(area, type) {
+      const existingArea = this.areas.find(item => item.area === area);
+      if (!existingArea) {
+          this.areas.push({ area, type, lastUpdated: null });
+          this.fetchWeatherData(area);
+      }
+  }
+
+  removeArea(area) {
+      const index = this.areas.findIndex(item => item.area === area);
+      if (index !== -1) {
+          this.areas.splice(index, 1);
+      }
+  }
+
+  fetchWeatherData(area) {
+      // Fetch weather data and update lastUpdated timestamp
+      // ... (Use a similar approach as before with fetch)
+      // Once you have the data, update the lastUpdated timestamp
+      const areaItem = this.areas.find(item => item.area === area);
+      areaItem.lastUpdated = new Date();
+  }
+}
+
+//select 3 buttons separately call add event listener on each
+//give them callback functions
+//save area button call selectedAreas
+//"new york" currentWeather .area  dot notation
+//
+
+
+
+
+const selectedAreas = new SelectedAreas();
+
+// Add an area as "home"  
+selectedAreas.addArea('New York', 'home');
+
+// Add an area as "saved"
+selectedAreas.addArea('Los Angeles', 'saved');
+
+// Remove an area
+selectedAreas.removeArea('New York');
+
+// Refetch weather information for selected areas
+function refetchSelectedAreasWeather() {
+  for (const areaItem of selectedAreas.areas) {
+      selectedAreas.fetchWeatherData(areaItem.area);
+  }
+}
+
+// Example of how to use the timestamp for last update
+const newYorkArea = selectedAreas.areas.find(item => item.area === 'New York');
+if (newYorkArea) {
+  console.log(`Last updated for New York: ${newYorkArea.lastUpdated}`);
+}
+
+// You can call refetchSelectedAreasWeather when needed to update weather data for selected areas.
